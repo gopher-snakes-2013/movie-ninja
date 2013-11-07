f7b563c59f6ab1ee0160144dcfc85f09ae5e87fd
Charles Amoako <Charles@Charless-MacBook-Pro.local>
2013-11-07 11:38:38 -0800

    initial rails skeleton setup

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.14'
+gem 'pg'
+gem 'jquery-rails'
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'shoulda-matchers'
+  gem 'simplecov'
+end
+
+group :development, :test do
+  gem 'rspec-rails'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieNinja::Application.config.secret_token = '846798054379858439141167976603f9e1ad0e815fd38f5db45801921b465965b5503848d4f1b6b7abe51229bbc2da2f633957a794dd977833e52dc8f9bdf412'
