7ed1f413217cfab8647e10859e8ad6542f186d27
Apprentice <apprentice@dbc-23.local>
2013-11-10 13:00:53 -0800

    fixed associations in movie and showtime models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,7 @@ class Movie < ActiveRecord::Base
 
   validates :title, presence: :true
 
-  has_many :survey_movies, :showtimes
+  has_many :survey_movies
+  has_many :showtimes
   has_many :surveys, through: :survey_movies
 end
