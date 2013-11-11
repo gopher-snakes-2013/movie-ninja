00be380b7a03d4994f2bafd6222319e749283124
gary1410 <gary@mobileworks.com>
2013-11-10 20:53:55 -0800

    able to add and remove movies from sidebar with icon in movie box

--- app/assets/javascripts/add_movie.js
+++ app/assets/javascripts/add_movie.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
+  var pickedMovies = []
+
   $('.select-movie').click(function(){
-    console.log('click registered')
-    var sideBarTemplate = $(this).parent().parent().find('.sidebar-movie-template').clone().removeClass('hidden')
-    $('#picked-movies').append(sideBarTemplate)
-    $(this).find('i').fadeTo(1,1)
+
+    $currentMovieBox = $(this).parent().parent()
+
+    if (pickedMovies.indexOf($currentMovieBox.data('id')) === -1) {
+
+      var sideBarTemplate = $currentMovieBox.find('.sidebar-movie-template').clone().removeClass('hidden')
+      $('#picked-movies').append(sideBarTemplate)
+
+      $(this).fadeTo(1,1)
+
+      pickedMovies.push($currentMovieBox.data('id'))
+    }
+  })
+
+  $('#picked-movies').on('click', 'i', function(){
+    var movieId = $(this).parent().data('id')
+    pickedMovies.splice(pickedMovies.indexOf(movieId), 1)
+    $($('[data-id =' + movieId + ']')[0]).find('.select-movie').fadeTo(1,.3)
+    $(this).parent().remove()
   })
 })
 
