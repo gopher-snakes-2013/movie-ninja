163f309a4b75581d0b5584075815320a211bd0b4
Apprentice <apprentice@dbc-23.local>
2013-11-11 18:44:37 -0800

    refactored survey.js into separate event listeners more styling on survey create (semi-decent color scheme now) renamed picked-movies to selected-movies

--- app/assets/javascripts/add_movie.js
+++ app/assets/javascripts/add_movie.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
-  var pickedMovies = []
+  // var pickedMovies = []
 
-  $('.select-movie').click(function(){
+  // $('.enqueue-movie-button').click(function(){
 
-    $currentMovieBox = $(this).parents('.movie-box')
+  //   $currentMovieBox = $(this).parents('.movie-box')
 
-    // if clicked movie is already in pickedMovies
-    if (pickedMovies.indexOf($currentMovieBox.data('id')) === -1) {
+  //   // if clicked movie is already in pickedMovies
+  //   if (pickedMovies.indexOf($currentMovieBox.data('id')) === -1) {
 
-      var sideBarTemplate = $currentMovieBox.find('.sidebar-movie-template').clone().removeClass('hidden')
-      $('#picked-movies').append(sideBarTemplate)
+  //     var sideBarTemplate = $currentMovieBox.find('.sidebar-movie-template').clone().removeClass('hidden')
+  //     $('#picked-movies').append(sideBarTemplate)
 
-      $(this).fadeTo(1,1)
+  //     $(this).fadeTo(1,1)
 
-      pickedMovies.push($currentMovieBox.data('id'))
-    }
-  })
+  //     pickedMovies.push($currentMovieBox.data('id'))
+  //   }
+  // })
 
   $('#picked-movies').on('click', 'i', function(){
     var movieId = $(this).parent().data('id')
     pickedMovies.splice(pickedMovies.indexOf(movieId), 1)
-    $($('[data-id =' + movieId + ']')[0]).find('.select-movie').fadeTo(1,.3)
+    $($('[data-id =' + movieId + ']')[0]).find('.enqueue-movie-button').fadeTo(1,.3)
     $(this).parent().remove()
   })
 })
--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -1,33 +1,69 @@
-$(document).ready(function(){
-    var moviePreviewVisible = false;
-    $('.movie-box-poster').click(function(event){
+var Survey = {
+  bindEventListeners: function(){
+   $('.movie-box-poster').click(function(event){
       event.preventDefault();
-      var id = $(this).children(":first").data('id');
-      $.ajax({
-        url: "/preview_movie",
-        type: "get",
-        data: { 'movie_id': id }
-      }).done(function(movie_preview_response){
-        console.log(movie_preview_response)
-        $('#movie-preview-container').html(movie_preview_response);
-        if(!moviePreviewVisible){
-          $('#movie-preview-container').toggle("slide", {direction: "left"});
-          moviePreviewVisible = true;
-          $('#sidebar').toggle("slide", {direction: "right"});
-        };
-      });
+      Survey.previewMovie($(this).children(":first").data('id'));
     });
-
-    // $(".movie-box[data-id=" + id + "]").toggleClass('selected');
-    $('#toggle-sidebar-button').click(function(event){
+   $('#toggle-sidebar-button').click(function(event){
       event.preventDefault();
-      $('#sidebar').toggle("slide", {direction: "right"});
-      $('#movie-preview-container').toggle("slide", {direction: "left"});
-      if(moviePreviewVisible){
-         moviePreviewVisible = false;
-      }
-      else{
+      Survey.toggleSideBar();
+    });
+   $('.enqueue-movie-button').click(function(event){
+      event.preventDefault();
+      console.log(this);
+      Survey.addMovieToSurvey($(this).data('id'));
+   });
+   $('#selected-movies').on('click', 'i', function(event){
+      event.preventDefault();
+      Survey.removeMovieFromSurvey($(this).data('id'));
+   });
+  },
+
+  previewMovie: function(movieId){
+    $.ajax({
+      url: "/preview_movie",
+      type: "get",
+      data: { 'movie_id': movieId }
+    }).done(function(movie_preview_response){
+      console.log(movie_preview_response)
+      $('#movie-preview-container').html(movie_preview_response);
+      if(!moviePreviewVisible){
+        $('#movie-preview-container').toggle("slide", {direction: "left"});
         moviePreviewVisible = true;
+        $('#sidebar').toggle("slide", {direction: "right"});
       };
     });
+  },
+
+  toggleSideBar: function(){
+    $('#sidebar').toggle("slide", {direction: "right"});
+    $('#movie-preview-container').toggle("slide", {direction: "left"});
+    if(moviePreviewVisible){
+       moviePreviewVisible = false;
+    }
+    else{
+      moviePreviewVisible = true;
+    };
+  },
+
+  addMovieToSurvey: function(movieId){
+    if(selectedMovies.indexOf(movieId) === -1) {
+      var title = $(".movie-box[data-id=" + movieId + "] > .movie-box-title").text();
+      $('#selected-movies').append("<div><i class='fa fa-times remove-movie'  data-id=" + movieId + "></i>" + title + "</div>");
+      $('[data-id='+movieId+']').find('.enqueue-movie-button').fadeTo(1,1)
+      selectedMovies.push(movieId);
+    };
+  },
+
+  removeMovieFromSurvey: function(movieId){
+    selectedMovies.splice(selectedMovies.indexOf(movieId), 1);
+    $('[data-id='+movieId+']').find('.enqueue-movie-button').fadeTo(1,.3)
+    $('#selected-movies').find('[data-id=' + movieId + ']').parent().remove();
+  }
+};
+
+$(document).ready(function(){
+  moviePreviewVisible = false;
+  selectedMovies = []
+  Survey.bindEventListeners();
 })
