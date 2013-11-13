11649e7eff58dc4d3fdd54af66ece6fd04186c5d
Apprentice <apprentice@dbc-23.local>
2013-11-12 21:22:01 -0800

    made movie-votes partial for survey confirmation to display votes

--- app/models/survey_movie.rb
+++ app/models/survey_movie.rb
@@ -1,5 +1,5 @@
 class SurveyMovie < ActiveRecord::Base
-
+  has_many :votes
   belongs_to :survey
   belongs_to :movie
 
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,5 @@ class Vote < ActiveRecord::Base
   attr_accessible :name
 
   belongs_to :survey_movie
-
   validates :name, presence: :true
 end
